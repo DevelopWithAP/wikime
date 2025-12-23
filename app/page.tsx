94d8bb955a9bc7426ac5346112c62e4f7b1b8b95
DevelopWithAP <apostolisparga@gmail.com>
2025-12-23 11:54:20 +0200

    Fix class name and layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import { NavBar } from "@/components/nav/nav-bar";
 import { WikiCard } from "@/components/ui/wiki-card";
 
 export default function Home() {
   return (
     <div>
-      <NavBar />
-      <main className="max-w-2-xl mx-auto mt-10 flex flex-col gap-6">
+      <main className="max-w-2xl mx-auto mt-10 flex flex-col gap-6">
         <WikiCard
           title="Entry one"
           author="Simba the One"
